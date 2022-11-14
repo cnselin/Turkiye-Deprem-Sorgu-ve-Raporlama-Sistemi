636b69011e4a8bea896df659644d1fdcbb720b05
Ali can <95063590+kaplancanali@users.noreply.github.com>
2022-11-14 21:45:15 +0300

    Update arayuz.h
    14 kasım en guncel hali

--- arayuz.h
+++ arayuz.h
@@ -1,24 +1,23 @@
-#include<stdio.h>
+#include<stdio.h> //standart kutuphane
 
-int menu ()
+int menu ()  //menu fonksiyonu
 {
     int secim=0;
    
     printf("\n");
-    printf("     TURKIYE DEPREM RAPOR VE SORGULAMA SISTEMINE HOS GELDINIZ!     \n"); 
+    printf("     TURKIYE DEPREM RAPOR VE SORGULAMA SISTEMINE HOS GELDINIZ!     \n"); //uygulama girisi
     printf("\n");
     
-    printf("(1) SORGULAMA\n");
-    printf("(2) SIRALAMA\n"); 
-    printf("(3) KAYDETME\n");
-    printf("(4) AYRISTIRMA\n");
-    printf("(5) ANALIZ\n");
-    printf("(6) CIKIS\n");
-    printf("SECIMINIZ: ");
+    printf("(1) SORGULAMA ISLEMI\n");  //kullanıcının secebilecegi secimler
+    printf("(2) SIRALAMA ISLEMI\n"); 
+    printf("(3) KAYDETME ISLEMI\n");
+    printf("(4) AYRISTIRMA ISLEMI\n");
+    printf("(5) ANALIZ ISLEMI\n");
+    printf("(6) CIKIS ISLEMI\n");
+    printf("SECIMINIZ: "); //kullanici secim yapti
     scanf("%d",&secim);
     
     
 
     return secim;
 }
-
